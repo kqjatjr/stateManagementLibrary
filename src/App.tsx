54f0ddaa4d6bb4fcf162de2bbceddec8418c091c
bskim <bskim2@rsupport.com>
2023-03-30 11:23:41 +0900

    jotai useAtom 사용해보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useAtom } from "jotai";
+import "./App.css";
+import { countAtom } from "./store/atoms";
 
 function App() {
+  const [numberAtom, setNumberAtom] = useAtom(countAtom);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div>{numberAtom}</div>
+        <div>
+          <button onClick={() => setNumberAtom((prev) => prev + 1)}>
+            증가
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
