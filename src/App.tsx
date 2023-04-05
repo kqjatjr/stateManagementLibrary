15bd5bdd183b4c565b1cc12f2e2d42cc477f0889
bskim <bskim2@rsupport.com>
2023-04-05 15:49:44 +0900

    리덕스 데브툴 연동해보기, devtool에서 action type을 명시적으로 수동 정의해보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 import useStore from "./store/store";
 
 function App() {
-  const { count, increment, initCount } = useStore((state) => state);
+  const [targetNumber, setTargetNumber] = useState(0);
+  const { count, increment, initCount, incrementByNumber } = useStore(
+    (state) => state,
+  );
   return (
     <div className="App">
       <div>카운트 : {count}</div>
       <div>
         <button onClick={() => initCount()}>초기화</button>
         <button onClick={() => increment()}>증가</button>
+        <div>
+          <input
+            placeholder="증가시킬 숫자"
+            type="number"
+            onChange={(e) => {
+              setTargetNumber(Number(e.target.value));
+            }}
+          />
+          <button onClick={() => incrementByNumber(targetNumber)}>
+            증가(target)
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,33 @@
 import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
 type Store = {
   count: number;
+  incrementByNumber: (by: number) => void;
   increment: () => void;
   initCount: () => void;
 };
 
-const useStore = create<Store>((set) => ({
-  count: 0,
-  increment: () => set((state) => ({ count: state.count + 1 })),
-  initCount: () => set({ count: 0 }),
-}));
+const useStore = create<Store>()(
+  devtools(
+    persist(
+      (set) => ({
+        count: 0,
+        incrementByNumber: (by) =>
+          set(
+            (state) => ({ count: state.count + by }),
+            false,
+            "TARGET_INCREMENT",
+          ),
+        increment: () =>
+          set((state) => ({ count: state.count + 1 }), false, "INCREMENT"),
+        initCount: () => set({ count: 0 }, false, "INIT_STATE"),
+      }),
+      {
+        name: "storeTest",
+      },
+    ),
+  ),
+);
 
 export default useStore;
